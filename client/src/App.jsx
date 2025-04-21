824bb8977188df0960a3e122ae1fe837db3c438b
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-04-22 03:21:23 +0600

    all page create done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Navbar from '../components/Navbar';
-import { Routes,Route } from 'react-router-dom';
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import Setting from "../pages/Setting";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      
+
       <Routes>
-<Route path="/" element={Home} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );

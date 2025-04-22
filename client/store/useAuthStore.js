62b36bf955b007ee85eb40839ab6c6150809b3fe
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-04-23 02:26:36 +0600

    login page profile page and sidebar done

--- client/store/useAuthStore.js
+++ client/store/useAuthStore.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 
 import { axiosInstance } from "./../lib/axios";
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
